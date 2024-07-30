c0166f16f7e1c7b5c9b5fc179bd0aaa1b2b68f9d
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-07-30 17:54:43 +0530

    dk

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -16,7 +16,7 @@ export async function GET(request: Request) {
 
 	if (code) {
 		const cookieStore = cookies();
-		const supabase = createServerClient(
+		const supabase = createServerClient<Database>(
 			process.env.NEXT_PUBLIC_SUPABASE_URL!,
 			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 			{
