ce384fc42c525099e5ce391563ad717a8c08e555
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-07-30 17:58:42 +0530

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
-// import { Database } from "@/lib/types/supabase";
+import { Database } from "@/lib/types/supabase";
 export async function GET(request: Request) {
 	const requestUrl = new URL(request.url);
 	const isAuth = cookies().get("supabase-auth-token");
