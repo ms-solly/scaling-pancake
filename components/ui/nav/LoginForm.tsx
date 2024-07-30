f277b82e0f5844b0a7acdbf5b016947431f0ef82
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-07-30 17:06:25 +0530

    Update LoginForm.tsx

--- components/ui/nav/LoginForm.tsx
+++ components/ui/nav/LoginForm.tsx
@@ -1,25 +1,33 @@
 "use client";
-import react from "react";
-import { Button } from "../button";
+import { Button } from "@/components/ui/button";
 import { SiGithub } from "react-icons/si";
 import { createBrowserClient } from "@supabase/ssr";
 import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function LoginForm() {
-    const pathname = usePathname();
-    const supabase = createBrowserClient(
+	const pathname = usePathname();
+	const supabase = createBrowserClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 	);
-    const handleLogin = () => {
-        supabase.auth.signInWithOAuth({
-            provider:"github",
-            options: {
-                redirectTo: process.env.PROD_URL + "auth/callback?next=" + pathname,
-            },
-        });
-    };
-    return (
-        <Button variant="outline" className="flex items-center gap-2" onClick={handleLogin}>< SiGithub />Login</Button>
-    )
+
+	const handleLogin = () => {
+		supabase.auth.signInWithOAuth({
+			provider: "github",
+			options: {
+				redirectTo: `${location.origin}/auth/callback?next=${pathname}`,
+			},
+		});
+	};
+
+	return (
+		<Button
+			className="flex items-center gap-2"
+			variant="outline"
+			onClick={handleLogin}
+		>
+			<SiGithub /> Login
+		</Button>
+	);
 }
